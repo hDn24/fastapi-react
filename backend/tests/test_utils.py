2d655d39560ccd7a6b7167dad56ea520459668a8
hDn24 <hodangnhan24@gmail.com>
2023-03-02 15:34:11 +0700

    Done CI

--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -1,17 +1,18 @@
 import random
 
-from app.utils import random_string
+# from app.utils import random_string
+import utils
 
 
 class TestRandomString:
     def test_length(self):
         length = random.randint(1, 100)
-        str1 = random_string(length)
+        str1 = utils.random_string(length)
         # sanity check
         assert length == len(str1)
 
     def test_different_each_time(self):
-        str1 = random_string(5)
-        str2 = random_string(5)
+        str1 = utils.random_string(5)
+        str2 = utils.random_string(5)
         # check two calls return different strings
         assert 2 == len(set([str1, str2]))
